d9b386493e28b14f3ef2c09b91e460642965a703
jungsm <seonmunjeong95@ggmail.com>
2023-05-31 00:12:03 +0900

    next.js 각 페이지 생성

---
+++ app/cart/payment/page.js
@@ -0,0 +1,10 @@
+
+
+export default function payment() {
+    return (
+      <div className="title">
+          <h1>결제페이지입니다</h1>
+      </div>
+    )
+  }
+  
--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
-
+import Link from "next/link"
 
 export default function Home() {
+  let name = '프로젝트'
   return (
     <div>
-      <h4 className="title">정선문</h4>
-      <p className="title-sub">Next.js 프로젝트</p>
+      <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/list">List</Link>
+      </div>
+      <div className="title">정선문</div>
+      <h4 className="title-sub">next.js {name}</h4>
     </div>
   )
 }
