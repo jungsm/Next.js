136b9643435952fba7b71954f3d192ccb46d66ff
jungsm <seonmunjeong95@ggmail.com>
2023-05-31 15:11:31 +0900

    플러스 마이너스 구현

--- app/list/page.js
+++ app/list/page.js
@@ -1,17 +1,44 @@
+'use client'
 
+import { useState } from "react";
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta' ,'Coconut'];
-
+  let [수량,수량변경] = useState([0,5,1])
     return (
       <div>
         <h4 className="title">상품목록</h4> 
+        <span>{수량[0]}</span>
+        <button onClick={()=>{ 
+          let copy = [...수량]
+          copy[0]++
+          수량변경(copy)
+          }}>+</button>
+        <span>{수량[1]}</span>
+        <button onClick={()=>{ 
+          let copy = [...수량]
+          copy[1]++
+          수량변경(copy)
+          }}>+</button>
+        <span>{수량[2]}</span>
+        <button onClick={()=>{ 
+          let copy = [...수량]
+          copy[2]++
+          수량변경(copy)
+          }}>+</button>
         {
           상품.map((a,i)=>{
             return (
               <div className="food" key={i}>
                 <img src={`/food${i}.png`} className="food-img"/>
               <h4>{a} &$40</h4>
+              <span>{수량[i]}</span>
+              <button onClick={()=>{let plus = [...수량]
+              plus[i]++
+              수량변경(plus)}}>+</button>
+              <button onClick={()=>{let minus = [...수량]
+              minus[i]--
+              수량변경(minus) }}>-</button>
           </div>
             )
           })
